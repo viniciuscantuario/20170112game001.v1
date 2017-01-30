b36f917094aa32629f9763bb7e7991b2eea6a8ec
Cesar Garcia <cesar@twohills.tv>
2017-01-30 10:33:25 -0200

    Objetos elipticos
    Adicionando formas elípticas aos objetos, Planeta, Orbita, Orbitando.
    (Toda melhoria esta comentada)

--- js/index.js
+++ js/index.js
@@ -9,13 +9,18 @@ var planet = {
     x: W / 2,
     y: H  - 30,
     r: W / 3,
+    //Raios necessarios para objetos elipticos
+    //rx: W / 1.5,
+    //ry: W / 3,
     update: function(){
     },
     render: function(){
         //PLANETA
         ctx.beginPath();
-        ctx.fillStyle = "#F4F4F4";
+        ctx.fillStyle = "blue";
         ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        //adicionando funcao para planeta eliptico
+        //ctx.ellipse(this.x, this.y, this.rx, this.ry, 0, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
     }
@@ -26,6 +31,9 @@ var orbit = {
     x: planet.x,
     y: planet.y,
     r: planet.r + (H / 8) * 0.5,
+    //Raios necessarios para objetos elipticos
+    //rx: planet.rx + (H / 8) * 0.5,
+    //ry: planet.ry + (H / 8) * 0.5,
     orbitVel: 3,
     update: function(){
         this.orbitVel += 0.01;
@@ -35,15 +43,19 @@ var orbit = {
         ctx.beginPath();
         ctx.setLineDash([5, 5]);
         ctx.lineWidth = 0.5;
-        ctx.strokeStyle = "#CCC";
+        ctx.strokeStyle = "gray";
         ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        //adicionando funcao para orbita eliptica
+        //ctx.ellipse(this.x, this.y, this.rx, this.ry, 0, 0, Math.PI * 2);
         ctx.stroke();
         ctx.closePath();
         
         //OBJETO ORBITANDO
         ctx.beginPath();
-        ctx.fillStyle = "#CCC";
+        ctx.fillStyle = "gray";
         ctx.arc(this.x + Math.cos(this.orbitVel) * (this.r), this.y + Math.sin(this.orbitVel) * (this.r), 20, 0, Math.PI * 2);
+        //Adicionando funcao para orbitar de forma eliptica
+        //ctx.ellipse(this.x + Math.cos(this.orbitVel) * (this.rx), this.y + Math.sin(this.orbitVel) * (this.ry), 20, 20, 0, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();       
     }
