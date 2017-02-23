78fd72ecd604ccb94a215f666466b1ddef770db7
Cesar Garcia <cesar@twohills.tv>
2017-02-23 20:44:17 -0300

    Basic Updates

--- js/index.js
+++ js/index.js
@@ -1,91 +1,218 @@
-//INICIALIZANDO VARIÁVEIS GLOBAIS
+//Declaração das variáveis iniciais
 var canvas,
     ctx,
-    W = 340,
-    H = 600;
+    W               = 340,
+    H               = 600,
+    status          = 2,
+    allStatus       = {
+        pres:       0,
+        menu:       1,
+        play:       2,
+        lose:       3
+    },
+    hpBarWidth      = W - 40, //Barra de hp do inimigo
+    newHpBarWidth   = W - 40, //Barra de hp do inimigo, Variável auxiliar
+    humanAttack     = 5;
+    planet          = [], 
+    enemie          = [],
+    planetId        = 0, //Define qual planet está em uso
+    orbitId         = 1, //Iniciar este parametro {orbitId} em 1 para que a orbita nunca toque o planeta
+    enemieId        = 0,
+    enemieBird      = new Image();
 
-//OBJETO PLANETA
-var planet = {
-    x: W / 2,
-    y: H  - 30,
-    r: W / 3,
-    //Raios necessarios para objetos elipticos
-    //rx: W / 1.5,
-    //ry: W / 3,
+//Declaração de atributos a variáveis gerais (img, audio etc)
+enemieBird.src  = "img/spritesheet_bird01.png";
+
+/**
+* Definições dos planetas
+* Cada planeta deverá ser definido como uma posição na matriz {planet}
+*/
+planet[0] = {
     update: function(){
     },
     render: function(){
-        //PLANETA
         ctx.beginPath();
-        ctx.fillStyle = "blue";
-        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
-        //adicionando funcao para planeta eliptico
-        //ctx.ellipse(this.x, this.y, this.rx, this.ry, 0, 0, Math.PI * 2);
+        ctx.fillStyle = "lightGrey";
+        ctx.arc(W/2, H, W/3, Math.PI, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
     }
-}
+};
+planet[1] = {
+    update: function(){
+    },
+    render: function(){
+        ctx.beginPath();
+        ctx.fillStyle = "grey";
+        ctx.arc(W/2, H, W/3, Math.PI, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+    }
+};
 
-//OBJETO ORBITA
+/**
+* Definições da orbita
+* A chave {orbitId} define qual nível da orbita vai ser usada
+*/
 var orbit = {
-    x: planet.x,
-    y: planet.y,
-    r: planet.r + (H / 8) * 0.5,
-    //Raios necessarios para objetos elipticos
-    //rx: planet.rx + (H / 8) * 0.5,
-    //ry: planet.ry + (H / 8) * 0.5,
-    orbitVel: 3,
+    x: W / 2,
+    y: H,
+    r1: W / 3,
+    r2: W / 3,
+    h2: H/8,
+    i: orbitId,
     update: function(){
-        this.orbitVel += 0.01;
+        this.o += 0.01;
+        if(this.o > Math.PI * 2){
+            this.o = Math.PI;
+        }
     },
     render: function(){
-         //ORBITA
+        ctx.setLineDash([1, 5]);
         ctx.beginPath();
-        ctx.setLineDash([5, 5]);
-        ctx.lineWidth = 0.5;
-        ctx.strokeStyle = "gray";
-        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
-        //adicionando funcao para orbita eliptica
-        //ctx.ellipse(this.x, this.y, this.rx, this.ry, 0, 0, Math.PI * 2);
-        ctx.stroke();
-        ctx.closePath();
-        
-        //OBJETO ORBITANDO
+        ctx.strokeStyle = "#34e89e";
+        ctx.ellipse(W/2, H - this.h2 * orbitId, W/2, W/3, 0, Math.PI, Math.PI * 2);
+        ctx.stroke();  
+        ctx.setLineDash([0]);
+    }
+ }
+
+/**
+* Definições dos inimigos
+* Cada inimigo deverá ser definido como uma posição na matriz {enemie}
+* O parâmetro {orbitId} define em qual nível da orbita ele vai aparecer
+*/
+enemie[0] = {
+    o: Math.PI,
+    f: 0,
+    w: 0,
+    hp: 20,
+    oldHp: 20,
+    update: function(){
+        this.o += 0.004;
+        if(this.o > Math.PI * 2){
+            this.o = Math.PI;
+        }
+        this.w++;
+        if(this.w >= 7){
+            if(this.f >= 2000){
+                this.f = 0;
+            }else{
+                this.f += 400;
+            }
+            this.w = 0;
+        }
+    },
+    render: function(){
+        ctx.drawImage(enemieBird, this.f, 0, 400, 372, (W/2 + Math.cos(this.o) * (W/2)) - 10, (H - (orbit.h2 * orbitId)) + Math.sin(this.o) * (W/3) - 9, 20, 18.5);
+    }
+}
+enemie[1] = {
+    o: Math.PI,
+    hp: 200,
+    oldHp: 200,
+    update: function(){
+        this.o += 0.01;
+        if(this.o > Math.PI * 2){
+            this.o = Math.PI;
+        }
+    },
+    render: function(){   
+    
         ctx.beginPath();
-        ctx.fillStyle = "gray";
-        ctx.arc(this.x + Math.cos(this.orbitVel) * (this.r), this.y + Math.sin(this.orbitVel) * (this.r), 20, 0, Math.PI * 2);
-        //Adicionando funcao para orbitar de forma eliptica
-        //ctx.ellipse(this.x + Math.cos(this.orbitVel) * (this.rx), this.y + Math.sin(this.orbitVel) * (this.ry), 20, 20, 0, 0, Math.PI * 2);
+        ctx.fillStyle = "#CCC";
+        ctx.arc(W/2 + Math.cos(this.o) * (W/2), (H - (orbit.h2 * orbitId)) + Math.sin(this.o) * (W/3), 10, 0, Math.PI * 2);
         ctx.fill();
-        ctx.closePath();       
+        ctx.closePath();
     }
 }
 
-//ATUALIZAÇÕES GERAIS
-function update(){
-    planet.update();
-    orbit.update();
+//Função temporária para mudar manualmente a orbita, planet ou inimigos
+function changeId(param, id){
+    if(param == "planet"){
+        planetId = id;
+    }else if(param == "orbit"){
+        orbitId = id;
+    }else if(param == "enemie"){
+        enemieId = id;
+        enemie[id].o = Math.PI;
+    }else if(param == "status"){
+        status = id;
+    }
 }
 
-//RENDERIZAÇÕES GERAIS
+
+//Função para renderizar a barra de hp do inimigo
+function renderHpBar(){
+    ctx.lineJoin = "round";
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "orange";
+    ctx.strokeRect(20, 20, newHpBarWidth,  1);
+    ctx.lineWidth = "1";
+    ctx.fillStyle = "white";
+    ctx.fillText(enemie[enemieId].hp, 25, 23);  
+}
+
+//Função para calcular o quanto da barra deve ser reduzida de acordo com o ataque do personagem e hp do inimigo
+function reductionHpBar(){
+    percentual = (humanAttack * 100) / enemie[enemieId].oldHp;
+    result = (hpBarWidth / 100) * percentual;
+    newHpBarWidth -= result;
+    enemie[enemieId].hp -= humanAttack;
+    if(newHpBarWidth <= 0){
+        newHpBarWidth = hpBarWidth; 
+        enemie[enemieId].hp = enemie[enemieId].oldHp;  
+    }
+    
+}
+
+//Recupera evento de clique do mouse
+function mouseDown(e){
+    reductionHpBar();
+}
+
+//Atualizações gerais do jogo
+function update(){
+    if(status == allStatus.pres){
+    }else if(status == allStatus.menu){
+    }else if(status == allStatus.play){
+        planet[planetId].update();
+        orbit.update();
+        enemie[enemieId].update();
+    }else if(status == allStatus.lose){
+    }
+}
+
+//Renderização dos elementos do jogo
 function render(){
     ctx.clearRect(0, 0, W, H);
-    planet.render();
-    orbit.render();
+    if(status == allStatus.pres){
+    }else if(status == allStatus.menu){
+    }else if(status == allStatus.play){
+        renderHpBar();
+        planet[planetId].render();
+        enemie[enemieId].render();
+        orbit.render();
+    }else if(status == allStatus.lose){
+    }
 }
 
+//Cria o loop do jogo
 function loop(){
     update();
     render();
     window.requestAnimationFrame(loop);
 }
 
-//INICIA JOGO
+//Função principal - e definições gerais do canvas e contexto
 function app(){
     canvas = document.createElement('canvas');
     canvas.width = W;
     canvas.height = H;
+    canvas.style.border = "1px solid #F4F4F4";
     ctx = canvas.getContext('2d');
     document.body.appendChild(canvas);
+    window.canvas.addEventListener('mousedown', mouseDown);
+    ctx.translate(0.5, 0.5);
     loop();
 }
